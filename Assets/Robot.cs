c8210644212044f2b0fa7cfa85ef9e91bff3ca84
John VanZwieten <john.vanzwieten@outlook.com>
2020-09-02 10:43:13 -0600

    baked enemy navmesh

---
+++ Assets/Robot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    public Transform Goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = Goal.position;
+    }
+
+    void Update()
+    {
+        
+    }
+}
